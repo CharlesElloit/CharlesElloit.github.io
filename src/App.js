8089b9ada9219cb99233e471cf7c701438adf110
CharlesElloit <aciforcharles@gmail.com>
2021-06-21 15:46:28 -0700

    added updateworkspace and deleteworkspace routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Home from "pages/home/Home";
+import CreateWorkspace from "components/workspace/CreateWorkspace";
+import UpdateWorkspace from "components/workspace/UpdateWorkspace";
+import DeleteWorkspace from "components/workspace/DeleteWorkspace";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/workspaces/add" component={CreateWorkspace} />
+        <Route exact path="/:name/update/:id" component={UpdateWorkspace} />
+        <Route exact path="/:name/delete/:id" component={DeleteWorkspace} />
       </Switch>
     </Router>
   );
