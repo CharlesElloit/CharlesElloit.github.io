70943eceaa628b437d9348828ae0d2137a91cd02
CharlesElloit <aciforcharles@gmail.com>
2021-06-26 01:28:00 -0700

    updated background color

--- src/pages/home/Home.styles.js
+++ src/pages/home/Home.styles.js
@@ -3,7 +3,7 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 const useStyles = makeStyles((theme) => ({
   container: {
     width: "100%",
-    height: "100%",
+    height: "100vh",
     background: "#0d1117",
   }
 }));
