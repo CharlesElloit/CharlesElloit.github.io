f5d2a26cef7d37a4f250b49e4ca8781eab813048
CharlesElloit <aciforcharles@gmail.com>
2021-07-11 13:46:26 -0700

    added splash loading screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React from "react";
 import Home from "pages/home/Home";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Loading from "components/loading/Loading";
 import CreateWorkspace from "components/workspace/CreateWorkspace";
 import UpdateWorkspace from "components/workspace/UpdateWorkspace";
 import DeleteWorkspace from "components/workspace/DeleteWorkspace";
 
 function App() {
+  const [progress, setProgress] = React.useState(0);
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    setLoading(true);
+    const timer = setInterval(() => {
+      setProgress(prevState => {
+        if (prevState === 100) {
+          setLoading(false)
+          return 0;
+        }
+        return Math.min(prevState + 15, 100);
+      });
+    }, 500);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  if (loading) {
+    return <Loading progress={progress} />
+  }
+
   return (
     <Router>
       <Switch>
